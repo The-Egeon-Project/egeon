d04cb6cd29b3bb387e5c53129b02a055dc9473cd
p4bl1sk1 <pablogerez2000@gmail.com>
2025-12-02 15:18:04 -0300

    setting up things

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,73 @@
+import { YtDlpPlugin } from '@distube/yt-dlp';
 import { Client, GatewayIntentBits } from 'discord.js';
+import { DisTube } from 'distube';
 import 'dotenv/config';
+
+const PLAY_COMMANDS = ['!play', '!p'];
+const STOP_COMMANDS = ['stop', 's'];
+const SKIP_COMMANDS = ['skip', 's'];
+const PAUSE_COMMANDS = ['pause', 'p'];
+const RESUME_COMMANDS = ['resume', 'r'];
+const QUEUE_COMMANDS = ['queue', 'q'];
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
   ],
 });
 
-client.on('ready', () => {
+const ytPlugin = new YtDlpPlugin({ update: true });
+const distube = new DisTube(client, { plugins: [ytPlugin] });
+
+client.on('clientReady', () => {
   console.log(`Bot logged in as ${client.user?.tag}`);
 });
 
-client.on('messageCreate', (msg) => {
-  if (msg.author.bot) return;
+client.on('messageCreate', (message) => {
+  if (!message.guild || !message.member || message.author.bot) {
+    console.log('No guild or member');
+    return;
+  }
 
-  if (msg.content === '!ping') {
-    msg.reply('Pong!');
+  const args = message.content.split(' ');
+  const command = args.shift()?.toLowerCase();
+
+  if (!command) {
+    console.log('No command');
+    return message.reply('Please provide a valid command.');
+  }
+
+  if (command === '!wallop') {
+    return message.reply('Hello World!');
+  }
+
+  if (PLAY_COMMANDS.includes(command)) {
+    const query = args.join(' ');
+
+    if (!query) {
+      return message.reply('Please provide a search query or URL.');
+    }
+
+    const voiceChannel = message.member.voice.channel;
+
+    if (!voiceChannel) {
+      return message.reply('You must be in a voice channel to play music.');
+    }
+
+    distube
+      .play(voiceChannel, query, {
+        message,
+        member: message.member,
+        textChannel: message.channel as any,
+      })
+      .catch((err) => {
+        console.error('Error playing song:', err);
+
+        return message.reply(err.message);
+      });
   }
 });
 
