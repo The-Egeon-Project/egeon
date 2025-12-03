24d704a6e27ad1512e68540a77226cd926a08ef2
p4bl1sk1 <pablogerez2000@gmail.com>
2025-12-02 21:37:10 -0300

    using lavalink

--- src/index.ts
+++ src/index.ts
@@ -1,84 +1,160 @@
-import { YtDlpPlugin } from '@distube/yt-dlp';
 import { Client, GatewayIntentBits } from 'discord.js';
-import { DisTube } from 'distube';
 import 'dotenv/config';
+import { Kazagumo, KazagumoTrack } from 'kazagumo';
+import { Connectors, NodeOption } from 'shoukaku';
 
-const PLAY_COMMANDS = ['!play', '!p'];
-const STOP_COMMANDS = ['stop', 's'];
-const SKIP_COMMANDS = ['skip', 's'];
-const PAUSE_COMMANDS = ['pause', 'p'];
-const RESUME_COMMANDS = ['resume', 'r'];
-const QUEUE_COMMANDS = ['queue', 'q'];
+const { Guilds, GuildVoiceStates, GuildMessages, MessageContent } =
+  GatewayIntentBits;
+
+const Nodes = [
+  {
+    name: process.env.LAVALINK_NAME!,
+    url: process.env.LAVALINK_URL!,
+    auth: process.env.LAVALINK_PASSWORD!,
+    secure: false,
+  },
+] as NodeOption[];
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+  intents: [Guilds, GuildVoiceStates, GuildMessages, MessageContent],
+});
+const kazagumo = new Kazagumo(
+  {
+    defaultSearchEngine: 'youtube',
+    // MAKE SURE YOU HAVE THIS
+    send: (guildId, payload) => {
+      const guild = client.guilds.cache.get(guildId);
+      if (guild) guild.shard.send(payload);
+    },
+  },
+  new Connectors.DiscordJS(client),
+  Nodes,
+);
+
+client.on('clientReady', () => console.log(client.user?.tag + ' Ready!'));
+
+kazagumo.shoukaku.on('ready', (name) =>
+  console.log(`Lavalink ${name}: Ready!`),
+);
+kazagumo.shoukaku.on('error', (name, error) =>
+  console.error(`Lavalink ${name}: Error Caught,`, error),
+);
+kazagumo.shoukaku.on('close', (name, code, reason) =>
+  console.warn(
+    `Lavalink ${name}: Closed, Code ${code}, Reason ${reason || 'No reason'}`,
+  ),
+);
+kazagumo.shoukaku.on('debug', (name, info) =>
+  console.debug(`Lavalink ${name}: Debug,`, info),
+);
+kazagumo.shoukaku.on('disconnect', (name, count) => {
+  const players = [...kazagumo.shoukaku.players.values()].filter(
+    (p) => p.node.name === name,
+  );
+  players.map((player) => {
+    kazagumo.destroyPlayer(player.guildId);
+    player.destroy();
+  });
+  console.warn(`Lavalink ${name}: Disconnected`);
 });
 
-const ytPlugin = new YtDlpPlugin({ update: true });
-const distube = new DisTube(client, { plugins: [ytPlugin] });
+kazagumo.on('playerStart', (player, track) => {
+  const channel = client.channels.cache.get(player.textId!) as any;
+  if (!channel) return;
 
-client.on('clientReady', () => {
-  console.log(`Bot logged in as ${client.user?.tag}`);
+  channel
+    .send({ content: `Now playing **${track.title}** by **${track.author}**` })
+    .then((message: any) => player.data.set('message', message));
 });
 
-client.on('messageCreate', (message) => {
-  if (!message.guild || !message.member || message.author.bot) {
-    console.log('No guild or member');
-    return;
+kazagumo.on('playerEnd', (player) => {
+  player.data.get('message')?.edit({ content: `Finished playing` });
+});
+
+kazagumo.on('playerEmpty', (player) => {
+  const channel = client.channels.cache.get(player.textId!) as any;
+  if (!channel) return;
+
+  channel
+    .send({ content: `Destroyed player due to inactivity.` })
+    .then((message: any) => player.data.set('message', message));
+
+  player.destroy();
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot || !message.guild || !message.member) return;
+
+  if (message.content.startsWith('!p') || message.content.startsWith('!play')) {
+    const args = message.content.split(' ');
+    const query = args.slice(1).join(' ');
+
+    const { channel } = message.member.voice;
+    if (!channel)
+      return message.reply(
+        'You need to be in a voice channel to use this command!',
+      );
+
+    let player = await kazagumo.createPlayer({
+      guildId: message.guild.id,
+      textId: message.channel.id,
+      voiceId: channel.id,
+      volume: 40,
+    });
+
+    let result = await kazagumo.search(query, { requester: message.author });
+    if (result.tracks[0] === undefined)
+      return message.reply('No results found!');
+
+    if (result.type === 'PLAYLIST')
+      player.queue.add(result.tracks); // do this instead of using for loop if you want queueUpdate not spammy
+    else player.queue.add(result.tracks[0]);
+
+    if (!player.playing && !player.paused) player.play();
+
+    return message.reply({
+      content:
+        result.type === 'PLAYLIST'
+          ? `Queued ${result.tracks.length} from ${result.playlistName}`
+          : `Queued ${result.tracks[0].title}`,
+    });
   }
 
-  const args = message.content.split(' ');
-  const command = args.shift()?.toLowerCase();
+  if (message.content.startsWith('!skip')) {
+    let player = kazagumo.players.get(message.guild.id);
+    if (!player) return message.reply('No player found!');
+    player.skip();
+    console.log(message.guild.id);
 
-  if (!command) {
-    console.log('No command');
-    return message.reply('Please provide a valid command.');
+    return message.reply({
+      content: `Skipped to **${player.queue[0]?.title}** by **${player.queue[0]?.author}**`,
+    });
   }
 
-  if (command === '!wallop') {
-    return message.reply('Hello World!');
+  if (message.content.startsWith('!forceplay')) {
+    let player = kazagumo.players.get(message.guild.id);
+    if (!player) return message.reply('No player found!');
+    const args = message.content.split(' ');
+    const query = args.slice(1).join(' ');
+    let result = await kazagumo.search(query, { requester: message.author });
+    const track = result.tracks[0] as any;
+
+    if (track === undefined) return message.reply('No results found!');
+
+    player.play(new KazagumoTrack(track.getRaw(), message.author));
+    return message.reply({
+      content: `Forced playing **${track.title}** by **${track.author}**`,
+    });
   }
 
-  if (PLAY_COMMANDS.includes(command)) {
-    const query = args.join(' ');
-
-    if (!query) {
-      return message.reply('Please provide a search query or URL.');
-    }
-
-    const voiceChannel = message.member.voice.channel;
-
-    if (!voiceChannel) {
-      return message.reply('You must be in a voice channel to play music.');
-    }
-
-    distube
-      .play(voiceChannel, query, {
-        message,
-        member: message.member,
-        textChannel: message.channel as any,
-      })
-      .catch((err) => {
-        console.error('Error playing song:', err);
-
-        return message.reply(err.message);
-      });
+  if (message.content.startsWith('!previous')) {
+    let player = kazagumo.players.get(message.guild.id);
+    if (!player) return message.reply('No player found!');
+    const previous = player.getPrevious(); // we get the previous track without removing it first
+    if (!previous) return message.reply('No previous track found!');
+    await player.play(player.getPrevious(true)); // now we remove the previous track and play it
+    return message.reply('Previous!');
   }
 });
 
 client.login(process.env.DISCORD_TOKEN);
-
-const shutdown = () => {
-  console.log('\nShutting down bot...');
-  client.destroy();
-  process.exit(0);
-};
-
-// Graceful shutdown
-process.on('SIGINT', shutdown);
-process.on('SIGTERM', shutdown);
